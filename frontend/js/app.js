2c73208caaf5fe3aa133cf9b4364a190815d16bc
MarcinBeck <marcin.beck@gmail.com>
2025-11-12 09:59:16 +0100

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,9 +3,9 @@
 // ====================================================================
 
 // DashboardDataResolver (GET - Ładowanie statystyk)
-const DASHBOARD_API_ENDPOINT = 'https://amw32505t5.execute-api.eu-north-1.amazonaws.com/default/ChapterManager'; 
+const DASHBOARD_API_ENDPOINT = 'https://1ciutze8te.execute-api.eu-north-1.amazonaws.com/default/DashboardDataResolver'; 
 // ChapterManager (POST - Dodawanie rozdziału, wywoływanie analizy)
-const CHAPTER_MANAGER_ENDPOINT = 'https://o7y6i29q3e.execute-api.eu-north-1.amazonaws.com/PlotAss'; 
+const CHAPTER_MANAGER_ENDPOINT = 'https://amw32505t5.execute-api.eu-north-1.amazonaws.com/default/ChapterManager'; 
 
 document.addEventListener('DOMContentLoaded', fetchData);
 
